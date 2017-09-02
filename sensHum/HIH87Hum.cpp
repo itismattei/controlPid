0a1f143ec6fa967eebfddd33b1698f0441c9bd31
itismattei <HTaAC_0703>
2017-09-02 14:05:31 +0200

    Creato nuovo branch per test sensori di umidita'

---
+++ sensHum/HIH87Hum.cpp
@@ -0,0 +1,14 @@
+/*
+ * HIH87Hum.cpp
+ *
+ *  Created on: 02 set 2017
+ *      Author: lenovoMax
+ */
+
+#include "HIH87Hum.h"
+
+HIH8_7Hum::HIH8_7Hum() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ sensHum/HIH87Hum.h
@@ -0,0 +1,16 @@
+/*
+ * HIH87Hum.h
+ *
+ *  Created on: 02 set 2017
+ *      Author: lenovoMax
+ */
+
+#ifndef SENSHUM_HIH87HUM_H_
+#define SENSHUM_HIH87HUM_H_
+
+class HIH8_7Hum {
+public:
+	HIH8_7Hum();
+};
+
+#endif /* SENSHUM_HIH87HUM_H_ */
